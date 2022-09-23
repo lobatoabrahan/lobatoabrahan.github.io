02264c83539a076e24622a2833f19689359b71b0
lobatoabrahan <83128157+lobatoabrahan@users.noreply.github.com>
2022-09-23 11:12:11 -0400

    modified

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+document.querySelector('.navbar-toggler').removeAttribute('aria-expanded')
 
 /* document.querySelector('.navbar-toggler').addEventListener('click', ()=>{
     document.querySelector('.navbar-toggler').setAttribute('aria-expandedd', 'true')
@@ -19,5 +19,6 @@ document.querySelector('.navbar-toggler').addEventListener('click', ()=>{
  document.querySelector('.navbar-collapse').addEventListener('click', ()=>{
     document.querySelector('.navbar-collapse').classList.remove('show');
     document.querySelector('.navbar-toggler').setAttribute('aria-expandedd', 'false')
+    document.querySelector('.navbar-toggler').setAttribute('aria-expanded', 'false')
  })
 
