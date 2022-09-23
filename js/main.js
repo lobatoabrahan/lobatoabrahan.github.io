a0f7fc663a518e5c07a61c2e0c6bfbdbad9831ee
lobatoabrahan <83128157+lobatoabrahan@users.noreply.github.com>
2022-09-23 11:05:12 -0400

    modified

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+/* document.querySelector('.navbar-toggler').addEventListener('click', ()=>{
+    document.querySelector('.navbar-toggler').setAttribute('aria-expandedd', 'true')
+}) */
+
+document.querySelector('.navbar-toggler').addEventListener('click', ()=>{
+    //Animate Links
+    console.log(document.querySelector('.navbar-toggler').getAttribute('aria-expandedd'));
+    console.log(document.querySelector('.navbar-collapse').className)
+
+    if (document.querySelector('.navbar-toggler').getAttribute('aria-expandedd') === 'false') {
+        document.querySelector('.navbar-toggler').setAttribute('aria-expandedd', 'true');
+    } else {
+        document.querySelector('.navbar-toggler').setAttribute('aria-expandedd', 'false')
+    }
+ });
+
+ document.querySelector('.navbar-collapse').addEventListener('click', ()=>{
+    document.querySelector('.navbar-collapse').classList.remove('show');
+    document.querySelector('.navbar-toggler').setAttribute('aria-expandedd', 'false')
+ })
+
